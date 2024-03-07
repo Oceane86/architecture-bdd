b2b3978415a4b5201aa30df61dc57b62cabc7f11
Votre nom <votre-email@example.com>
2024-03-07 11:32:11 +0100

    Suppressions fichiers

--- README.md
+++
@@ -1,2 +0,0 @@
-# architecture-bdd
-# architecture-bdd
--- tests/test-exo1.py
+++
@@ -1,9 +0,0 @@
-
-chaine = input("Entrez une chaine de caractères: ")
-
-majuscules = 0
-minuscules = 0
-
-
-print("Nombre de majuscules:", majuscules)
-print("Nombre de minuscules:", minuscules)
