f8c1a25952ff5cd08e76fbff83b6d6cc2202500f
Oceane86 <124265385+Oceane86@users.noreply.github.com>
2024-03-08 11:47:30 +0100

    Update test_exo10.py

--- tests/test_exo10.py
+++ tests/test_exo10.py
@@ -1,3 +1,7 @@
+# Écrire un programme qui choisit un nombre aléatoire entre 1 et 100 et demande à l'utilisateur de le deviner. L'utilisateur a droit à un nombre limité de tentatives. 
+# Après chaque tentative, le programme indique si le nombre mystère est plus grand ou plus petit que la saisie de l'utilisateur.
+
+
 import pytest
 
 
