3bd430685a235cbced2f7b39c4b22a4754295bd8
Votre nom <votre-email@example.com>
2024-03-08 09:47:54 +0100

    Your commit message

---
+++ tests/test_exo0.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+def addition(a, b):
+    return a + b
+
+
+@pytest.mark.parametrize("a, b, r", [(10, 8, 18), (-5, -10, -15)])
+def test_add_two_numbers(a, b, r):
+    print(f"La somme de a={a}, b={b}, est égale à={r}")
+    assert addition(a, b) == r
