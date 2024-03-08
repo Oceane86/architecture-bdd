4d58728b5819669d9d15558c9ab5b55dfd291272
Votre nom <votre-email@example.com>
2024-03-08 11:22:52 +0100

    Exo 0 : String

--- tests/test_exo0.py
+++ tests/test_exo0.py
@@ -1,11 +1,13 @@
-import unittest
+# Écrivez un programme Python qui accepte une chaîne de caractères de l'utilisateur et affiche le nombre de majuscules et de minuscules.
+
 import pytest
 
 
 def addition(a, b):
     return a + b
 
-@pytest.mark.parametrize("a, b, r", [(10, 8, 18), (-5, -10, -15), (2.5, 3.5, 6.0)])
+
+@pytest.mark.parametrize("a, b, r", [(10, 8, 18), (-5, -10, -15)])
 def test_add_two_numbers(a, b, r):
-        print(f"La somme de a={a}, b={b}, est égale à={r}")
-        assert addition(a, b) == r
+    print(f"La somme de a={a}, b={b}, est égale à={r}")
+    assert addition(a, b) == r
