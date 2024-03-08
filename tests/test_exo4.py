2f1977ef4249237b1435410a03e72bcaff95a485
Votre nom <votre-email@example.com>
2024-03-08 11:30:02 +0100

    Exo 2 : String : Update test_exo3.py test_exo4.py

--- tests/test_exo4.py
+++ tests/test_exo4.py
@@ -1,3 +1,8 @@
+# Vous disposez de la chaîne de caractères suivante : "Python est un langage de programmation puissant et facile à apprendre."
+# Extraction simple : Extraire le mot "Python" de la chaîne.
+# Extraction par indices négatifs : Extraire le mot "apprendre" en utilisant des indices négatifs.
+# Slicing : Extraire la phrase "langage de programmation" en utilisant le slicing.
+# Challenge : Inversez la chaîne de caractères entière.
 import pytest
 
 
